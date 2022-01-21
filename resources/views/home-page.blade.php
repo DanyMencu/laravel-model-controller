a34f71e78a8f31aacb0e4141a07ea2ba1e507914
DanyMencu <dany69men@gmail.com>
2022-01-21 17:52:08 +0100

    Add little scss style

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            @foreach ($movies as $movie)
-            <div class="col-4 movie-card">
-                <ul>
-                    <li>
-                        <strong>Titolo del film:</strong> {{ $movie->title }}
-                    </li>
-                    <li>
-                        <strong>Titolo in lingua originale:</strong> {{ $movie->original_title }}
-                    </li>
-                    <li>
-                        <strong>Nazionalità del film:</strong> {{ $movie->nationality }}
-                    </li>
-                    <li>
-                        <strong>Data di pubblicazione:</strong> {{ $movie->date }}
-                    </li>
-                    <li>
-                        <strong>Voto del film:</strong> {{ $movie->vote }}
-                    </li>
-                </ul>
+    <section class="movies-section py-3">
+        <div class="container">
+            <div class="row justify-content-center">
+                @foreach ($movies as $movie)
+                <div class="col-3 p-2 movie-card">
+                    <ul>
+                        <li>
+                            <strong>Titolo del film:</strong> {{ $movie->title }}
+                        </li>
+                        <li>
+                            <strong>Titolo in lingua originale:</strong> {{ $movie->original_title }}
+                        </li>
+                        <li>
+                            <strong>Nazionalità del film:</strong> {{ $movie->nationality }}
+                        </li>
+                        <li>
+                            <strong>Data di pubblicazione:</strong> {{ $movie->date }}
+                        </li>
+                        <li>
+                            <strong>Voto del film:</strong> {{ $movie->vote }}
+                        </li>
+                    </ul>
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-    </div>
+    </section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="bg-dark py-3">
     <div class="container">
         <div class="text-center">
             <div>Footer content</div>
